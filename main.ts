54e77cea35830db9b639c1c916e96ff674f2cf18
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-24 11:22:50 -0400

    make the code

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo
+ * Created on: Sep 2024
+ * This program combertsd from celcius to kelvin 
 */
 
-basic.showString('Hello, World!')
+// comberts form celcius to kelvin 
+let currentTemperature: number 
+let kelvin: number 
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    currentTemperature = input.temperature()
+   kelvin = (currentTemperature + 273.15)
+   kelvin = Math.round(kelvin)
+   basic.showString('The temperature is')
+   basic.showNumber(kelvin)
+   basic.showString('k')
+    basic.clearScreen()
+})
+
